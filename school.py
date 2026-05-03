0d7e1fbe7392e54c3e52e6259a480c89bc3ea004
arfinaleusuf <arfinaleusuf@gmail.com>
2026-05-03 23:29:22 +0600

    grade to value

--- school.py
+++ school.py
@@ -29,3 +29,16 @@ class School:
             return 'D'
         else:
             return 'F'
+        
+    @staticmethod
+    def grade_to_value(grade):
+        grade_map = {
+            'A+' : 5.00,
+            'A' : 4.00,
+            'A-' : 3.50,
+            'B' : 3.00,
+            'C' : 2.00,
+            'D' : 1.00,
+            'F' : 0.00
+        }
+        return grade_map[grade]
