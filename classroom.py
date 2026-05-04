e376cc793370f8de41371f4ddd44ee2144c1da08
arfinaleusuf <arfinaleusuf@gmail.com>
2026-05-04 16:32:38 +0600

    classroom

---
+++ classroom.py
@@ -0,0 +1,16 @@
+class ClassRoom:
+    def __init__(self, name):
+        self.name = name
+        self.students = []  # list of studnet object
+        self.subjects = []  # list of subject object
+    def add_student(self, student):  #rahim, eight e vhorti honbe.
+        roll_no = f"{self.name} - {len(self.students) + 1}"
+        student.id = roll_no
+        self.students.append(student)
+    def add_subject(self, subject):
+        self.subjects.append(subject)
+    def take_semestar_final_exam(self):
+        for subject in self.subjects:
+            subject.exam(self.students)
+        for student in self.students:
+            student.calculate_final_grade()
